8b0074d6658d9fb163a49ad9ff960d22b5a02be6
indoor <thinghoon@icloud.com>
2024-10-18 17:58:35 +0900

    ch6

--- modern_java_in_action/src/main/java/org/example/ch6/notebook.java
+++ modern_java_in_action/src/main/java/org/example/ch6/notebook.java
@@ -1,9 +1,9 @@
 package org.example.ch6;
 
-import java.util.Comparator;
-import java.util.IntSummaryStatistics;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 import static org.example.ch6.Dish.menu;
 
@@ -35,5 +35,34 @@ public class notebook {
         // 6.2.3 문자열 연결
         String shortMenu = menu.stream().map(Dish::getName).collect(Collectors.joining(", "));
         System.out.println(shortMenu);
+
+        // 6.2.4 범용 리듀싱 요약 연산
+        int totalCalories2 = menu.stream().collect(Collectors.reducing(0, Dish::getCalories, (x, y) -> x + y));
+        System.out.println(totalCalories2);
+
+        Optional<Dish> mostCalorieDish2 = menu.stream().collect(Collectors.reducing(
+                (d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)
+        );
+        System.out.println(mostCalorieDish2.orElse(null));
+
+        // collect와 reduce
+        Stream<Integer> stream = Arrays.asList(1, 2, 3, 4, 5, 6).stream();
+        List<Integer> numbers = stream.reduce(
+                new ArrayList<Integer>(),
+                (List<Integer> l, Integer e) -> {
+                    l.add(e);
+                    return l;
+                },
+                (List<Integer> l1, List<Integer> l2) -> {
+                    l1.addAll(l2);
+                    return l1;
+                }
+        );
+
+        // 6.3 그룹화
+        Map<Dish.Type, List<Dish>> dishesByType = menu.stream().collect(Collectors.groupingBy(Dish::getType));
+        System.out.println(dishesByType);
+
+
     }
 }
